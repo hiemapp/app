7c65830f075f0af565ce454eee25d09d0394ccb8
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-05-29 08:51:01 +0200

    chore: remove console.log() statement

--- server/src/trpc/routers/flow.ts
+++ server/src/trpc/routers/flow.ts
@@ -13,7 +13,5 @@ export const flowRouter = router({
             console.log({ a: input.workspace })
 
             flow.updateWorkspace(input.workspace);
-
-            console.log({ input, flow });
         })
 })
