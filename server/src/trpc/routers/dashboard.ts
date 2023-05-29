d11a2d47947a62d61ec2593aac00b1901f190326
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-05-29 22:28:55 +0200

    feat: session support for dashboard widgets
    Session support uses one instance of a dashboard widget per user
    and stores this instance in memory. This means that a widget can now
    hold a state.

--- server/src/trpc/routers/dashboard.ts
+++ server/src/trpc/routers/dashboard.ts
@@ -1,20 +1,40 @@
 import { router, publicProcedure } from '../trpc';
-import { ExtensionController, DashboardWidget } from 'zylax';
 import _ from 'lodash';
 import { z } from 'zod';
-import { serializeWidgetNode } from 'zylax';
+import { ExtensionController, renderWidgetAndSerialize } from 'zylax';
+import { utils, DashboardWidget } from 'zylax';
+
+const widgetSessions: Record<string, DashboardWidget> = {};
 
 export const dashboardRouter = router({
     getRenderedWidget: publicProcedure
         .input(z.object({
-            slug: z.string() 
+            slug: z.string()
         }))
         .query(({ ctx, input }) => {
-            const widget = ExtensionController.findModule(DashboardWidget, input.slug);
+            const widgetSessionId = `${input.slug}#user${ctx.user.getId()}`;
+            const widgetType = ExtensionController.findModule(DashboardWidget, input.slug);
+            
+            // Find the widget by the session id
+            let widget = widgetSessions[widgetSessionId];
+
+            // If no widget exists for this session, create it
+            if(!widget) {
+                // Construct the widget
+                widget = new widgetType(widgetSessionId);
+
+                // Store the widget by the session id
+                widgetSessions[widgetSessionId] = widget;
+
+                // Call the mount listener
+                widget.onMount();
+            }
+
 
             return {
-                manifest: widget.prototype.getManifest(),
-                content: serializeWidgetNode(widget.prototype.render())
+                sessionId: widgetSessionId,
+                manifest: widget.getManifest(),
+                content: renderWidgetAndSerialize(widget)
             };
         })
 })
