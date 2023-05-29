3bcb25471bea658139ab0eb95566a57b302fcfad
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-05-29 19:43:16 +0200

    feat: add parseColor() function to parse various color strings

--- client/src/utils/colors.ts
+++ client/src/utils/colors.ts
@@ -8,5 +8,15 @@ export function getColorValue(color: (typeof colorpalettes)[0][0]): string {
     return BODY_COMPUTED_STYLE.getPropertyValue(`--${color.name}-${color.intensity}`);
 }
 
+export function parseColor(color: string, lightness: number = 5) {     
+    if(color === 'inherit' || color === 'initial' || color === 'unset')
+        return color;
+
+    if(colorpalettes[color])
+        return colorpalettes[color][lightness];
+
+    return color;
+}
+
 const colors = {};
 export default colors;
