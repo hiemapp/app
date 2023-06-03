20958a1abecd7055388c7678e57bbd1a9a74a16c
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-06-03 22:24:32 +0200

    refactor: move input logic to Devices page

--- client/src/Devices/pages/Devices.page.tsx
+++ client/src/Devices/pages/Devices.page.tsx
@@ -1,37 +1,45 @@
 import { Page, Container } from '@tjallingf/react-utils';
+import { useMemo } from 'react';
 import Masonry from '@/Masonry';
 import Device from '@/Devices/components/Device/Device';
 import ErrorBoundary from '@/ErrorBoundary';
 import { trpc } from '@/utils/trpc';
+import useSocketEvent from '@/hooks/useSocketEvent';
 
 const Devices: React.FunctionComponent = () => {
-    const devices = trpc.device.list.useQuery();
-    // let { data: rooms } = useQuery<IRoomProps[]>(['rooms']);
+    const devicesQuery = trpc.device.list.useQuery();
+    const deviceInputMutation = trpc.device.handleInput.useMutation();
 
-    const renderDevices = () => {
-        if(devices.isLoading || !devices.data) return;
-        // if(typeof rooms == 'undefined' || !activeRoom) return null;
+    useSocketEvent('devices:change', () => {
+        devicesQuery.refetch();
+    });
 
-        // const filteredDevices = devices!.filter(props =>
-        //     rooms[3].devices.includes(props.id));
+    function handleInput(id: number, name: string, value: any) {
+        deviceInputMutation.mutate({ 
+            id: id, 
+            values: [{ name, value }]
+        });
+    }
+
+    const devices = useMemo(() => {
+        if(devicesQuery.isLoading || !devicesQuery.data) return;
 
         return (
             <Masonry
                 breakpointCols={{ default: 6, 1200: 5, 992: 3, 768: 3, 576: 2, 575.98: 1 }}
-                className="flex-row align-items-start"
-            >
-                {devices.data.map((props: any) => (
-                    <ErrorBoundary key={props.id}>
-                        <Device {...props} />
+                className="flex-row align-items-start">
+                {devicesQuery.data.map((data: any) => (
+                    <ErrorBoundary key={data.id}>
+                        <Device data={data} handleInput={(n, v) => handleInput(data.id, n, v)} />
                     </ErrorBoundary>
                 ))}
             </Masonry>
         );
-    };
+    }, [ devicesQuery.data ]);
 
     return (
         <Page id="devices">
-            <Container>{renderDevices()}</Container>
+            <Container>{devices}</Container>
         </Page>
     );
 };
