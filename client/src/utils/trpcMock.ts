995e774bfa5b93ce905f6f0b68e0d8f039966ac9
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-06-03 16:47:20 +0200

    fix(types): add useQueries mock

--- client/src/utils/trpcMock.ts
+++ client/src/utils/trpcMock.ts
@@ -1,2 +1,8 @@
-export const trpc: any = {};
+interface TRPCMock {
+    useQueries: (cb: (...args: any[]) => any) => any[],
+    [key: string]: any
+}
+
+// @ts-ignore
+export const trpc: TRPCMock = {};
 export const trpcClient: any = {};
