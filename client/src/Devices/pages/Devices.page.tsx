b7a0db5d615d68587c1843d8d8901c40ed3f753d
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-06-04 20:43:18 +0200

    BREAKING CHANGE: pass props via the new data prop feat: add dataUpdatedAt prop for memoization

--- client/src/Devices/pages/Devices.page.tsx
+++ client/src/Devices/pages/Devices.page.tsx
@@ -1,5 +1,4 @@
 import { Page, Container } from '@tjallingf/react-utils';
-import { useMemo } from 'react';
 import Masonry from '@/Masonry';
 import Device from '@/Devices/components/Device/Device';
 import ErrorBoundary from '@/ErrorBoundary';
@@ -7,11 +6,16 @@ import { trpc } from '@/utils/trpc';
 import useSocketEvent from '@/hooks/useSocketEvent';
 
 const Devices: React.FunctionComponent = () => {
-    const devicesQuery = trpc.device.list.useQuery();
+    const deviceIndexQuery = trpc.device.index.useQuery();
+    const deviceQueries = trpc.useQueries(t => (
+        deviceIndexQuery.isSuccess
+            ? deviceIndexQuery.data.map(({ id }) => t.device.get({ id }))
+            : []
+    ))
     const deviceInputMutation = trpc.device.handleInput.useMutation();
 
-    useSocketEvent('devices:change', () => {
-        devicesQuery.refetch();
+    useSocketEvent('devices:change', ({ device }) => {
+        deviceQueries.find(q => q.data?.id === device.id)?.refetch?.();
     });
 
     function handleInput(id: number, name: string, value: any) {
@@ -21,25 +25,35 @@ const Devices: React.FunctionComponent = () => {
         });
     }
 
-    const devices = useMemo(() => {
-        if(devicesQuery.isLoading || !devicesQuery.data) return;
+    const devices = deviceQueries.map(query => {  
+        if(query.isLoading) {
+            return null;
+        }
+
+        if(query.isError || typeof query.data.id !== 'number') {
+            console.error('Failed to load device:', query.error);
+            return null;
+        }
 
         return (
-            <Masonry
-                breakpointCols={{ default: 6, 1200: 5, 992: 3, 768: 3, 576: 2, 575.98: 1 }}
-                className="flex-row align-items-start">
-                {devicesQuery.data.map((data: any) => (
-                    <ErrorBoundary key={data.id}>
-                        <Device data={data} handleInput={(n, v) => handleInput(data.id, n, v)} />
-                    </ErrorBoundary>
-                ))}
-            </Masonry>
-        );
-    }, [ devicesQuery.data ]);
+            <ErrorBoundary key={query.data.id}>
+                <Device 
+                    data={query.data} 
+                    dataUpdatedAt={query.dataUpdatedAt}
+                    handleInput={(n, v) => handleInput(query.data.id, n, v)} />
+            </ErrorBoundary>
+        )
+    })
 
     return (
         <Page id="devices">
-            <Container>{devices}</Container>
+            <Container>
+                <Masonry
+                    breakpointCols={{ default: 6, 1200: 5, 992: 3, 768: 3, 576: 2, 575.98: 1 }}
+                    className="flex-row align-items-start">
+                    {devices}
+                </Masonry>
+            </Container>
         </Page>
     );
 };
