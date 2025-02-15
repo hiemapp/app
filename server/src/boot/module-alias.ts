47dfd46c3d356d7e3837facd8d19ff31c3328598
Tjalling Feenstra <hello@tjalling.net>
2025-02-15 12:24:21 +0100

    fix: resolve core src dir correctly

--- server/src/boot/module-alias.ts
+++ server/src/boot/module-alias.ts
@@ -1,7 +1,13 @@
+import path from 'path';
 import moduleAlias from 'module-alias';
+import fs from 'fs';
 
 export async function boot(rootDir: string) {
     if(process.env.NODE_ENV === 'development') {
-        moduleAlias.addAlias('hiem', '../../core/src');
+        // Alias 'hiem' to the core source dir, if found
+        const CORE_SRC_DIR = path.resolve(rootDir, '../../core/src');
+        if(fs.existsSync(CORE_SRC_DIR)) {
+            moduleAlias.addAlias('hiem', CORE_SRC_DIR);
+        }
     }
 }
